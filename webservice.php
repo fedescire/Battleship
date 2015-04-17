824212d21cd7d0ff80d8e2c23381577b0da25452
marvinrott <marvinrott@hotmail.de>
2015-04-17 11:43:40 +0200

    v1.2
    Grundfunktionen fertig + getestet
    Rückgabewerte anhand Klassen geändert
    
    to do:
    abbrechen game
    get player info
    
    fehler:
    setfinishgame->timestamp begin wird überschrieben

--- webservice.php
+++ webservice.php
@@ -1,11 +1,14 @@
 <?php
 
 	include("class/dbclass.php");
+	include("class/game.php");
+	include("class/gameinfo.php");
+	include("class/playerinfo.php");
 	
 	$arr=null;
 	//Registrieren('Fede', 'Fedep');
 	//Login('Fede','Fedep');
-	//setSpiel('Fede', 'Spiel4');
+	//setSpiel('Marvin', 'Spiel5');
 	//JoinSpiel(3, 'Marvin');
 	//getSpiele();
 	//setSchiffPosition(1, 'Fede',1, 5, 5, 'h');
@@ -13,7 +16,7 @@
 	//setSpielZug(1);
 	//updateSpielZug(1,'Fede',5,5);
 	//getSpielZug(1);
-	//setFinishGame(3, "Marvin", 800);
+	//setFinishGame(4, "Marvin", 800);
 	//getHighscore();
 	//getMitspieler(1,'Fede');
 
@@ -28,7 +31,7 @@
 		$check=$xmlquery->prepare('Select use_name from tuser where use_name=?;')->set($Username)->execute();
 		if(isset($check[0]) && $check[0]->use_name==$Username)
 		{
-			return 'Username existiert bereits';
+			return 'Fehler';
 		}
 		else
 		{
@@ -40,13 +43,13 @@
 	{
 		$check=null;
 		$xmlquery=new dbclass();
-		$check=$xmlquery->prepare('Select use_name from tuser where use_name= ? and use_passwort=?;')->set($UserName,$Password)->execute();		if(isset($check[0]) && !is_null($check[0]->use_name) && $check[0]->use_name==$UserName)
+		$check=$xmlquery->prepare('Select use_name from tuser where use_name= ? and use_passwort=?;')->set($UserName,$Password)->execute();		
+		if(isset($check[0]) && !is_null($check[0]->use_name) && $check[0]->use_name==$UserName)
 		{
 			return 'ok';
 		}
 		else
 		{
-			echo "fehler";
 			return 'Fehler';
 		}
 	}
@@ -55,25 +58,31 @@
 	{
 		$xmlquery=new dbclass();
 		$tmp="0000-00-00 00:00:00";
-		$xmlquery->prepare('insert into tspiel values(?,?,?,?,?,?,?,?)')->set(null,$SpielName,null,null,null,$tmp,null,$SpielerName)->executeNonQuery();
+		$newspielid=$xmlquery->prepare('insert into tspiel values(?,?,?,?,?,?,?,?)')->set(null,$SpielName,null,null,null,$tmp,null,$SpielerName)->executeNonQuery();
+		return $newspielid;
 	}
 
 	function JoinSpiel($SpielID, $SpielerName)
 	{
 		$xmlquery=new dbclass();
 		$xmlquery->prepare('update tspiel set spi_fgast_name=? where spi_id=?;')->set($SpielerName,$SpielID)->executeNonQuery();
+		echo 'ok';
+		//überprüfen ob funkt hat
 	}
 
 	function getSpiele() //Gibt Liste mit Spielen zurück
 	{
 		$xmlquery=new dbclass();
 		$arr=$xmlquery->prepare('Select spi_id, spi_name, spi_fhost_name from tspiel where spi_fgast_name is null')->set()->execute();
-		/*foreach($arr as $data){
-			echo $data->spi_id;
-			echo $data->spi_name;
-			echo $data->spi_fhost_name;
-		}*/
-		return $arr;
+		$gamearray=array();
+		foreach($arr as $data){
+			$tmp = new game();
+			$tmp->spielID=$data->spi_id;
+			$tmp->spielName=$data->spi_name;
+			$tmp->host=$data->spi_fhost_name;
+			$gamearray[]=$tmp;
+		}
+		return $gamearray;
 	}
 
 	function setSchiffPosition($SpielID, $SpielerName,$SchiffModel, $x, $y, $Richtung)
@@ -85,66 +94,83 @@
 	function getSchiffposition($SpielID, $GegnerName)
 	{
 		$xmlquery=new dbclass();
-		$arr=$xmlquery->prepare('Select schiffpos_x,schiffpos_y from tschiffpos where schiffpos_spi_fuser_name=? and schiffpos_spiel_id=?')->set($GegnerName,$SpielID)->execute();
-		/*foreach($arr as $data){
-			echo $data->schiffpos_x;
-			echo $data->schiffpos_y;
-		}*/
-		return $arr;
-	}
+		$arr=$xmlquery->prepare('Select schiffpos_x,schiffpos_y,schiffpos_model,schiffpos_richtung,schiffpos_spiel_id,schiffpos_spi_fuser_name from tschiffpos where schiffpos_spi_fuser_name=? and schiffpos_spiel_id=?')->set($GegnerName,$SpielID)->execute();
+		$schiffposarray=array();
+		foreach($arr as $data){
+			$tmp=new gameinfo();
+			$tmp->x_kord=$data->schiffpos_x;
+			$tmp->y_kord=$data->schiffpos_y;
+			$tmp->schiffmodell=$data->schiffpos_model;
+			$tmp->richtung=$data->schiffpos_richtung;
+			$tmp->spielid=$data->schiffpos_spiel_id;
+			$tmp->spieler=$data->schiffpos_spi_fuser_name;
+			$schiffposarray[]=$tmp;
+		}
+		return $schiffposarray;
+	}	
 
 	function setSpielZug($SpielID)
 	{
 		$xmlquery=new dbclass();
 		$xmlquery->prepare('insert into tspielzug values(?,?,?,?,?)')->set(null,null,null,null,$SpielID)->executeNonQuery();		
-		//sicherheitsabfrage schauen ob es schon gibt
 	}
 
 	function updateSpielZug($SpielID,$SpielerName,$x_pos,$y_pos)
 	{
 		$xmlquery=new dbclass();
 		$xmlquery->prepare('update tspielzug set spielzug_use_name=?, spielzug_x_pos=?, spielzug_y_pos=? where spielzug_spi_fid=?;')->set($SpielerName,$x_pos,$y_pos,$SpielID)->executeNonQuery();
-		//sicherheitsabfrage schauen ob es spielzug schon gibt
 	}
 
 	function getSpielZug($SpielID) //SpielerID (eigene) überprüft wer letzten Zug gemacht. Wenn !=SpielerID, dann wird ZUg zurück gegeben
 	{
 		$xmlquery=new dbclass();
 		$arr=$xmlquery->prepare('Select spielzug_use_name,spielzug_x_pos, spielzug_y_pos from tspielzug where spielzug_spi_fid='.$SpielID.';')->set()->execute();
-		/*foreach($arr as $data){
-			echo $data->spielzug_use_name;
-			echo $data->spielzug_x_pos;
-			echo $data->spielzug_y_pos;
-		}*/
-		return $arr;
+		$spielzugarray=array();
+		foreach($arr as $data){
+			$tmp=new gameinfo();
+			$tmp->spieler=$data->spielzug_use_name;
+			$tmp->x_kord=$data->spielzug_x_pos;
+			$tmp->y_kord=$data->spielzug_y_pos;
+			$spielzugarray[]=$tmp;
+		}
+		return $spielzugarray;
 	}	
 
 	function setFinishGame($SpielID, $GewinnerName, $SpielerPunkte)
 	{
+
+
+
 		$xmlquery=new dbclass();
 		$xmlquery->prepare('update tspiel set spi_gewinner_name=?, spi_gewinnerpunkte=?, spi_time_ende=null where spi_id=?;')->set($GewinnerName,$SpielerPunkte,$SpielID)->executeNonQuery();	
 		//timestamp beginn fehler
+
 	}
 
 	function getHighscore()
 	{
 		$xmlquery=new dbclass();
 		$arr=$xmlquery->prepare('Select use_name, spi_gewinnerpunkte from tspiel, tuser where spi_gewinner_name=use_name order by spi_gewinnerpunkte desc;')->set()->execute();
-		/*foreach($arr as $data){
-			echo $data->use_name;
-			echo $data->spi_gewinnerpunkte;
-		}*/
+		$highscorelist=array();
+		foreach($arr as $data){
+			$tmp=new playerinfo();
+			$tmp->username=$data->use_name;
+			$tmp->highscore=$data->spi_gewinnerpunkte;
+			$highscorelist[]=$tmp;
+		}
+		return $highscorelist;
 
-		return $arr;
 	}
 
 	function getMitspieler($SpielID,$SpielerName)
 	{
 		$xmlquery=new dbclass();
+		$tmp=new playerinfo();
 		$arr=$xmlquery->prepare('Select use_name from tuser,tspiel where spi_id=? and (spi_fgast_name!=? or spi_fhost_name!=? and use_name!=?);')->set($SpielID,$SpielerName,$SpielerName,$SpielerName)->execute();
-		$mitspieler= $arr[0]->use_name;
-		
-		return $mitspieler;
+
+		$tmp->username=$arr[0]->use_name;
+		var_dump($tmp);
+		return $tmp;
 	}
 
 
