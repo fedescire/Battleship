c4d9f1249a21ca30bb1eb65f16435c806aaf9fd4
marvinrott <marvinrott@hotmail.de>
2015-04-17 11:46:11 +0200

    v1
    für rückgabe

---
+++ gameinfo.php
@@ -0,0 +1,18 @@
+<?php 
+
+	class gameinfo
+	{
+		function __construct()
+		{
+		}
+
+		public $spieler;
+		public $x_kord;
+		public $y_kord;
+		public $schiffmodell;
+		public $richtung;
+		public $spielid;
+
+	}
+
+?>
