a86669e30b7048b8e0eee2d894669c9d0288065a
Nidodorfa <mair.94@live.de>
2015-05-05 10:05:12 +0200

    V 1.1
    Änderung bei SetFinishSpiel:
    Es wurde setFinishSpiel aufgerufen, funktion in der DB heist aber SetFinishGame ahbe es geändert

--- webservice.php
+++ webservice.php
@@ -1,82 +1,86 @@
 <?php
+
+/*
+*   Die Klasse battleship.php, ist ein Webservice, der Anfragen über GET bekommt, sie in einem Switch an die verlangte Funktion weitergibt 
+*   und dann die Werte die er bekommt in ein JSON umwandelt und sie dann an den Client zurück gibt.  
+*
+*/
+
 	if(!empty($_GET)){
 		$function = $_GET['funktion'];
-		
 		$return = null;
-			//include("webservice.php");
+        
+        
+			include("webservice.php");
 
 			function Answer($ans){
 
-					echo $ans;
-			}
+					$msg = json_encode($ans);
 
-			function setSpiel($SpielerID, $SpielName){
-				return  $SpielerID ." ". $SpielName;
+					echo $msg;
 			}
 
 			switch ($function) {
 					
 					case 'setSpiel':
-						$ans = setSpiel($_GET['SpielerID'], $_GET['SpielName']);
-						Answer($ans);
+		 				$return = setSpiel($_GET['SpielerID'], $_GET['SpielName']);
+						Answer($return);
+					break;
+                    case 'setSpielzug':
+                        $return = setSpielZug($_GET['SpielID']);
+						Answer($return);
 					break;
 					case 'getSpiele':
 						$return = getSpiele();
-						Answer();
+						Answer($return);
 					break;
 					case 'setSchiffPosition':
-						$return = setSchiffPosition($_GET['SpielID'], $_GET['SpielerID'], $_GET['Schiffmodell'],$_GET['x'], $_GET['y']);
-						Answer();
+						$return = setSchiffPosition($_GET['SpielID'], $_GET['SpielerID'], $_GET['Schiffmodell'],$_GET['x'], $_GET['y'],$_GET["direction"]);
+						Answer($return);
 					break;
 					case 'getSchiffPosition':
 						$return = getSchiffPosition($_GET['SpielID'],$_GET['GegnerID']);
-						Answer();
+						Answer($return);
 					break;
 					case 'joinSpiel':
 						$return = joinSpiel($_GET['SpielID'], $_GET['SpielerID']);
-						Answer();
+						Answer($return);
 					break;
 					case 'getMitspieler':
-						$return = getMitspieler($_GET['SpielID']);
-						Answer();
+						$return = getMitspieler($_GET['SpielID'],$_GET['Username']);
+						Answer($return);
 					break;
 					case 'getSpielZug':
 						$return = getSpielZug($_GET['SpielID'], $_GET['SpielerID']);
-						Answer();
+						Answer($return);
 					break;
 					case 'setSpielZug':
-						$return = setSpielZug($_GET['SpielID'], $_GET['SpielerID'], $_GET['x'], $_GET['y']);
-						Answer();
+						$return = updateSpielZug($_GET['SpielID'], $_GET['SpielerID'], $_GET['x'], $_GET['y']);
+						Answer($return);
 					break;
 					case 'getPlayerInfo':
 						$return = getPlayerInfo($_GET['SpielerID']);
-						Answer();
+						Answer($return);
 					break;
 					case 'getHighscore':
 						$return = getHighscore();
-						Answer();
+						Answer($return);
 					break;
 					case 'Login':
 						$return = Login($_GET['Username'], $_GET['Password']);
-						Answer();
+						Answer($return);
 					break;
 					case 'Registrieren':
 						$return = Registrieren($_GET['Username'],$_GET['Password']);
-						Answer();
+						Answer($return);
 					break;
 					case 'setFinishSpiel':
-						$return = setFinishSpiel($_GET['SpielID'],$_GET['GewinnerID'],$_GET['SpielerPunkte']);
-						Answer();
+						$return = setFinishGame($_GET['SpielID'],$_GET['GewinnerID'],$_GET['SpielerPunkte']);
+						Answer($return);
 					break;
-					case 'abbrechenSpiel':
-						$return = abbrechenSpiel($_GET['SpielID'],$_GET['AufgeberID']);
-						Answer();
-					break;
-				
 				default:
-
 					$return = "Fehler";
-					Answer();
+					Answer($return);
 					break;
 			}
 
